7e40178570eb3ef7dc123d60665e1f6792e8fe7a
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-02-20 20:25:33 -0500

    made spawners for all objects

--- Assets/Assignment 2 code/Spawner.cs
+++ Assets/Assignment 2 code/Spawner.cs
@@ -6,11 +6,38 @@ using UnityEngine.UI;
 public class Spawner : MonoBehaviour
 {
         public GameObject prefabAk;
+
         public GameObject prefabPistol;
+
+        public GameObject prefabSniper;
+
+        public GameObject prefabMiniGun;
+
+        public GameObject prefabLightSaber;
+
+        public GameObject prefabRPG ;
+
+        public GameObject prefabShotGun;
+
+        public GameObject prefabBlaster;
+
         public Button AK;
+
         public Button Pistol;
 
-        void Start()
+        public Button Sniper;
+
+        public Button MiniGun;
+
+        public Button LightSaber;
+
+        public Button RPG;
+
+        public Button Blaster;
+
+        public Button ShotGun;
+
+    void Start()
         {
 
         }
@@ -24,6 +51,31 @@ public class Spawner : MonoBehaviour
         {
             Instantiate(prefabPistol, new Vector3(0, 0, 0), Quaternion.identity);
         }
+        public void SpawnPrefabSniper()
+        {
+        Instantiate(prefabSniper, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    public void SpawnPrefabMiniGun()
+    {
+        Instantiate(prefabMiniGun, new Vector3(0, 0, 0), Quaternion.identity);
     }
+    public void SpawnPrefabLightSaber()
+    {
+        Instantiate(prefabLightSaber, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+    public void SpawnPrefabRPG()
+    {
+        Instantiate(prefabRPG, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+    public void SpawnPrefabShotGun()
+    {
+        Instantiate(prefabShotGun, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+    public void SpawnPrefabBlaster()
+    {
+        Instantiate(prefabBlaster, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
+
 
 
